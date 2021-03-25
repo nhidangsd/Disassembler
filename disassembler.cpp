1d0b8808a170e9c9c8b9c3297b96ba5a5909425c
Luka Jozic <Luka.033@hotmail.com>
2021-03-24 23:09:59 -0700

    Implemented textparser function along with a whole lot of helper functions.

--- disassembler.cpp
+++ disassembler.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <typeinfo>
+#include <iomanip>
+
 
 using namespace std;
 
@@ -48,6 +51,7 @@ void DisAssembler::ReadFiles(char* objFile)
     }
 }
 
+
 void DisAssembler::PrintText()
 {
     for (auto& line : objLines)
@@ -62,4 +66,308 @@ void DisAssembler::PrintText()
     
     
 }
+
+void DisAssembler::Parser()
+{
+    for (const string& line : objLines)
+    {
+        char firstChar = *line.c_str();
+        switch (firstChar)
+        {
+            case 'H':
+                HeaderParser(line.substr(1));
+                break;
+            case 'T':
+                TextParser(line.substr(1));
+                break;
+            case 'E':
+                EndParser(line.substr(1));
+                break;
+            default:
+                break;
+
+        }
+    }
+}
+
+void DisAssembler::HeaderParser(string line)
+{
+    string headerName = line.substr(0, 6);
+    int startingAddress = stoi(line.substr(9, 4), nullptr, 16);
+    WriteToLst(startingAddress, headerName, "", "START  0", "");
+}
+
+void DisAssembler::TextParser(string line)
+{
+    //cout << line << endl;
+    // 6910083E174000024000
+
+    int currentMemoryAddress = stoi(line.substr(2, 4), nullptr, 16);
+    long recordLength = HexToDecimal(line.substr(6, 2));
+    int format;
+    
+    //cout << "OPCODES: " << line.substr(8) << endl;
+
+    
+    for (int i = 8; i < recordLength * 2 + 8; )
+    {
+        //cout << "\nSTARTING ADDR: " << currentMemoryAddress << endl;
+        //cout << "MOST RECENT ADDR: " << mostRecentMemoryAddress << endl;
+
+        if (currentMemoryAddress - mostRecentMemoryAddress > 4)
+        {
+            //cout << "RESW IS NEEDED HERE =====================" << endl;
+            CheckMemoryGap(mostRecentMemoryAddress, currentMemoryAddress);
+        }
+
+        
+
+        string targetName = (symbolTable.find(currentMemoryAddress) != symbolTable.end()) ? symbolTable.at(currentMemoryAddress).first : "";
+        //cout << "TARGET NAME " << targetName << endl;
+
+        string firstBits = HexToBinary(line.substr(i, 3)); 
+        //cout << "FIRST BITS: " << firstBits << endl;
+
+
+        string mnemonic = GetMnemonic(firstBits.substr(0, 6)).first;
+        //cout << "MNEMONIC: " << mnemonic << endl;
+
+        if (GetMnemonic(firstBits.substr(0, 6)).second == 2)
+        {
+            //cout << "CUT HERE ==========================================" << endl;
+            format = 2;
+            string opCode = line.substr(i, format * 2);
+            int regIdx = (int)opCode.at(2) - 48;
+            //cout << "OPCODE: " << opCode << endl;
+            WriteToLst(currentMemoryAddress, targetName, mnemonic, registerTable.substr(regIdx, 1), opCode);
+        }
+        else
+        {
+            int nixbpe = stoi(firstBits.substr(6, 6));
+            //cout << "NIXBPE: " << std::dec << nixbpe << endl;
+
+
+            string asl = CalculateTargetAddress(nixbpe, 0).first;
+            //cout << "ASL: " << asl << endl;
+
+
+            format = asl.find('+') != string::npos ? 4 : GetMnemonic(firstBits.substr(0, 6)).second;
+            //cout << "FORMAT: " << format << endl;
+
+            PCRegister = currentMemoryAddress + format;
+            //cout << "PCRegister: " << PCRegister << endl;
+
+
+            unsigned int dispOrAddr = format == 4 ? stoi(line.substr(i - 1 + format, format + 1), nullptr, 16)
+                : stoi(line.substr(i + format, format), nullptr, 16);
+            //cout << "DISP OR ADDR: " << std::hex << dispOrAddr << endl;
+
+            unsigned int targetAddress = CalculateTargetAddress(nixbpe, dispOrAddr).second;
+            if (format == 3 && targetAddress > 0x1000)
+                targetAddress -= 0x1000;
+            //cout << "TARGET ADDRESS: " << std::hex << targetAddress << endl;
+
+            
+
+            string srcVarName = (symbolTable.find(targetAddress)) != symbolTable.end() ? symbolTable.at(targetAddress).first : "";
+            //cout << "SRC SYMBOL NAME: " << srcVarName << endl;
+            
+            string opCode = line.substr(i, format * 2);
+            //cout << "OPCODE: " << opCode << endl;
+
+            if (targetName.find(opCode) != string::npos)
+            {
+                WriteToLst("", "LTORG");
+                mnemonic = "*";
+                WriteToLst(currentMemoryAddress, "", "*", targetName, opCode);
+            } 
+            else
+            {
+                string srcStatement = asl.replace(asl.find("op"), 2, mnemonic);
+                if (asl.find('m') != string::npos)
+                {
+                    srcStatement = asl.replace(asl.find("m"), 1, srcVarName);
+                    //cout << "SRC STATEMENT: " << srcStatement << endl;
+                }
+                else if (asl.find('c') != string::npos)
+                {
+                    srcStatement = asl.replace(asl.find("c"), 1, opCode.substr(5));
+                    //cout << "SRC STATEMENT: " << srcStatement << endl;
+                }
+
+                WriteToLst(currentMemoryAddress,
+                    targetName, srcStatement.substr(0, srcStatement.find(" ")),
+                    srcStatement.substr(srcStatement.find(" ")),
+                    opCode);
+            }
+            UpdateRegisters(mnemonic, dispOrAddr);
+        }
+        currentMemoryAddress += format;
+        mostRecentMemoryAddress = currentMemoryAddress;
+        i += format * 2;
+    }
+}
+
+
+void DisAssembler::EndParser(string line)
+{
+    int endingAddress = stoi(line, nullptr, 16);
+    WriteToLst("END", symbolTable.at(endingAddress).first);
+}
+
+
+
+pair<string, unsigned int> DisAssembler::CalculateTargetAddress(int flagBits, unsigned int dispOrAddr)
+{
+    switch (flagBits)
+    {
+        case 110000:
+            return { "op c", dispOrAddr };
+        case 110001:
+            return { "+op m", dispOrAddr };
+        case 110010:
+            return { "op m", PCRegister + dispOrAddr };
+        case 110100:
+            return { "op m", baseRegister + dispOrAddr };
+        case 111000:
+            return { "op c, X", dispOrAddr + XRegister };
+        case 111001:
+            return { "+op m, X", dispOrAddr + XRegister };
+        case 111010:
+            return { "op m, X", PCRegister + dispOrAddr + XRegister };
+        case 100000:
+            return { "op @c", dispOrAddr };
+        case 100001:
+            return { "+op @m", dispOrAddr };
+        case 100010:
+            return { "op @m", PCRegister + dispOrAddr };
+        case 100100:
+            return { "op @m", baseRegister + dispOrAddr };
+        case 10000:
+            return { "op #c", dispOrAddr };
+        case 10001:
+            return { "+op #m", dispOrAddr };
+        case 10010:
+            return { "op #m", PCRegister + dispOrAddr };
+        case 10100:
+            return { "op #m", baseRegister + dispOrAddr };
+        default:
+            return { string(), 0 };
+    }
+}
+
+
+
+
+
+
+pair<string, int> DisAssembler::GetMnemonic(string binary)
+{
+    binary += "00";
+    string opCode = BinaryToHex(binary);
+    return { opCodeTable.at(opCode).first, opCodeTable.at(opCode).second };
+}
+
+
+void DisAssembler::CheckMemoryGap(int rangeLower, int rangeUpper)
+{
+    string mnemonic = "RESW";
+    vector<unsigned int> addressRanges;
+    for (const auto& key : symbolTable)
+    {
+        if (key.first >= rangeLower && key.first <= rangeUpper)
+        {
+            addressRanges.push_back(key.first);
+        }       
+    }
+
+    for (int i = 0; i < addressRanges.size() - 1; i++)
+    {
+        int currentMemoryAddress = addressRanges[i];
+        int currentRange = addressRanges[i + 1] - addressRanges[i];        
+        string targetName = symbolTable.at(addressRanges[i]).first;
+        WriteToLst(currentMemoryAddress,targetName, mnemonic, to_string(currentRange / 3), "");
+    }
+    int currentMemoryAddress = addressRanges[addressRanges.size() - 1];
+    int lastRange = rangeUpper - addressRanges[addressRanges.size()-1];
+    string targetName = symbolTable.at(addressRanges[addressRanges.size() - 1]).first;
+    if(lastRange != 0)
+        WriteToLst(currentMemoryAddress, targetName, mnemonic, to_string(lastRange / 3), "");
+
+}
+
+
+void DisAssembler::UpdateRegisters(string mnemonic, unsigned int value)
+{
+    if (mnemonic == "LDB" && !baseRegisterActive)
+    {
+        baseRegisterActive = true;
+        baseRegister = value;
+        WriteToLst("BASE", symbolTable.at(baseRegister).first);
+    }
+}
+
+pair<string, string> DisAssembler::GetAddressingMode(string flagBits)
+{
+    return { addressingModeTable.at(flagBits).first, addressingModeTable.at(flagBits).second };
+}
+
+
+/**
+    Converts a hexadecimal number to decimal number.
+    @param hexadecimal number to convert.
+    @return the hexadecimal number converted to decimal
+*/
+long DisAssembler::HexToDecimal(string hex)
+{
+    return stol(hex, nullptr, 16);
+}
+
+
+/**
+    Converts a hexadecimal number to binary number.
+    @param hexadecimal number to convert.
+    @return the hexadecimal number converted to binary
+*/
+string DisAssembler::HexToBinary(string hex)
+{
+    string binary = "";
+    for (const auto& character : hex)
+    {
+        int index = hexDigits.find(character);
+        binary += binaryNums[index];
+    }
+    return binary;
+}
+
+
+/**
+    Converts a binary number to hexadecimal number.
+    @param binary number to convert.
+    @return the binary number converted to hexadecimal
+*/
+string DisAssembler::BinaryToHex(string binary)
+{
+    string hex = "";
+    for (int i = 0; i < binary.size(); i +=4)
+    {
+        int index = find(binaryNums, binaryNums + BIN_LIST_SIZE, binary.substr(i, 4)) - binaryNums;
+        hex += hexDigits[index];  
+    }
+    return hex;
+}
+
+void DisAssembler::WriteToLst(int address, string name, string mnemonic, string srcName, string opcode)
+{
+    printf("%04X", address);
+    cout << setw(10) << name << setw(10) << mnemonic << setw(10) << srcName << setw(10) << opcode << endl;
+}
+
+void DisAssembler::WriteToLst(string mnemonic, string srcName)
+{
+    cout << setw(20) << mnemonic << setw(10) << srcName << endl;
+}
+
+
+
 
--- disassembler.h
+++ disassembler.h
@@ -1,7 +1,11 @@
 #include <vector>
 #include <string>
+#include <map>
+#include <algorithm>
 
 using namespace std;
+
+#define BIN_LIST_SIZE 16
 
 class DisAssembler 
 {
@@ -12,12 +16,95 @@ public:
     
     void ReadFiles(char* objFile);
     void PrintText();
+    void Parser();
+    void TextParser(string line);
     
+    pair<string, string> GetAddressingMode(string flagBits);
+    pair<string, unsigned int> CalculateTargetAddress(int flagBits, unsigned int dispOrAddr);
 
 private:
+    void HeaderParser(string line);
+    void EndParser(string line);
+    pair<string, int> GetMnemonic(string binary);
+
+    void CheckMemoryGap(int rangeLower, int rangeUpper);
+
+    void UpdateRegisters(string mnemonic, unsigned int value);
+    long HexToDecimal(string hex);
+    string HexToBinary(string hex);
+    string BinaryToHex(string binary);
+    void WriteToLst(int address, string name, string mnemonic, string srcName, string opcode);
+    void WriteToLst(string mnemonic, string srcName);
+
     vector<string> objLines;
     vector<string> symLines;
 
+
+    bool baseRegisterActive = false;
+    bool XRegisterFlag = false;
+    unsigned int baseRegister = 0;
+    unsigned int PCRegister = 0;
+    unsigned int XRegister = 0;
+    unsigned int ARegister = 0;
+
+    int mostRecentMemoryAddress;
+    
+    const string registerTable = "AXLBSTF";
+
+    map<string, pair<string, int>> opCodeTable = {
+        {"18", {"ADD", 3}},  {"58", {"ADDF", 3}},   {"90", {"ADDR", 2}},   {"40", {"AND", 3}},  {"B4", {"CLEAR", 2}},
+        {"28", {"COMP", 3}}, {"88", {"COMPF", 3}},  {"A0", {"COMPR", 2}},  {"24", {"DIV", 3}},  {"64", {"DIVF", 3}},
+        {"9C", {"DIVR", 2}}, {"C4", {"FIX", 1}},    {"C0", {"FLOAT", 1}},  {"F4", {"HIO", 1}},  {"3C", {"J", 3}},
+        {"30", {"JEQ", 3}},  {"34", {"JGT", 3}},    {"38", {"JLT", 3}},    {"48", {"JSUB", 3}}, {"00", {"LDA", 3}},
+        {"68", {"LDB", 3}},  {"50", {"LDCH", 3}},   {"70", {"LDF", 3}},    {"08", {"LDL", 3}},  {"6C", {"LDS", 3}},
+        {"74", {"LDT", 3}},  {"04", {"LDX", 3}},    {"D0", {"LPS", 3}},    {"20", {"MUL", 3}},  {"60", {"MULF", 3}},
+        {"98", {"MULR", 2}}, {"C8", {"NORM", 1}},   {"44", {"OR", 3}},     {"D8", {"RD", 3}},   {"AC", {"RMO", 2}},
+        {"4C", {"RSUB", 3}}, {"A4", {"SHIFTL", 2}}, {"A8", {"SHIFTR", 2}}, {"F0", {"SIO", 1}},  {"EC", {"SSK", 3}},
+        {"0C", {"STA", 3}},  {"78", {"STB", 3}},    {"54", {"STCH", 3}},   {"80", {"STF", 3}},  {"D4", {"STI", 3}},
+        {"14", {"STL", 3}},  {"7C", {"STS", 3}},    {"E8", {"STSW", 3}},   {"84", {"SIT", 3}},  {"10", {"STX", 3}},
+        {"1C", {"SUB", 3}},  {"5C", {"SUBF", 3}},   {"94", {"SUBR", 2}},   {"B0", {"SVC", 2}},  {"E0", {"TD", 3}},
+        {"F8", {"TIO", 1}},  {"2C", {"TIX", 3}},    {"B8", {"TIXR", 2}},   {"DC", {"WD", 3}},
+    };
+
+
+    map<string, pair<string, string>> addressingModeTable = {
+        {"110000", { "op c", "disp" }},
+        {"110001", { "+op m", "addr" }},
+        {"110010", { "op m", "(PC) + disp" }},
+        {"110100", { "op m", "(B) + disp" }},
+        {"111000", { "op c, X", "disp + (X)" }},
+        {"111001", { "+op m, X", "addr + (X)" }},
+        {"110010", { "op m, X", "(PC) + disp + (X)" }},
+        {"100000", { "op @c", "disp" }},
+        {"100001", { "+op @m", "addr" }},
+        {"100010", { "op @m", "(PC) + disp" }},
+        {"100100", { "op @m", "(B) + disp" }},
+        {"010000", { "op #c", "disp" }},
+        {"010001", { "+op #m", "addr" }},
+        {"010010", { "op #m", "(PC) + disp" }},
+        {"010100", { "op #m", "(B) + disp" }}
+    };
+    
+
+    map<unsigned int, pair<string, string>> symbolTable = {
+        {0x0, { "FIRST", "R" }},
+        {0xA, { "BADR", "R" }},
+        {0x83E, { "RETADR", "R" }},
+        {0x849, { "WLOOP", "R" }},
+        {0x85C, { "EADR", "R" }},
+
+        {0x855, { "=X'000001'", "6" }},
+        {0x1090, { "=X'000007'", "6" }}
+    };
+
+
+
+    const string binaryNums[BIN_LIST_SIZE] = { "0000", "0001", "0010", "0011", "0100", "0101","0110", "0111", "1000", "1001", "1010",
+                               "1011", "1100", "1101", "1110","1111" };
+    const string hexDigits = "0123456789ABCDEF";
+
+
 };
+
 
 
